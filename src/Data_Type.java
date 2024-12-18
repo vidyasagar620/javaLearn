c4ccd09b80edc9b0bd523c31abf0de823f96a1e3
VIDYA SAGAR YADAV <vs1262537vidya@gmail.com>
2024-12-18 22:15:20 +0530

    We are Learn How to Data Type Are Assign In java

---
+++ src/Data_Type.java
@@ -0,0 +1,14 @@
+public class Data_Type {
+    public static void main(String[] args) {
+        int a =5;
+        System.out.println(a);
+        float b = 4.22f;
+        System.out.println(b);
+        double c = 9.54848245d;
+        System.out.println(c);
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+        boolean man = true;
+        System.out.println(man);
+    }
+}
