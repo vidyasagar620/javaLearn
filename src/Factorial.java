ce95144d85cf7545eba412791dcdf9389b67e58a
vidyasagar620 <145008515+vidyasagar620@users.noreply.github.com>
2024-12-25 21:58:14 +0530

    We are Learn How to  Perform Factorial Edited  In java

--- src/Factorial.java
+++ src/Factorial.java
@@ -6,32 +6,24 @@ public class Factorial {
         System.out.println("Welcome to Factorial Calculator\n_____________________ ");
         System.out.print("Enter Number: ");
         int num = input.nextInt();
-        int Mul = FactorialNum(num);
+        long Mul = FactorialNum(num);
         System.out.println("Your Factorial is: " + Mul);
 
     }
-    public static int FactorialNum(int num){
-        Scanner input = new Scanner(System.in);
-        if ( num < 0){
-            System.out.println("negative Number is Not allowed");
-            System.out.print("Enter positive  Number: ");
-            int num2 = input.nextInt();
-            int Mul = 1;
-            int i = 1;
-            while (i <= num2){
-                Mul *= i;
-                i++;
-            }
-            return Mul;
-        }else {
-            int Mul = 1;
-            int i = 1;
-            while (i <= num){
-                Mul *= i;
-                i++;
-            }
-            return Mul;
-        }
 
+    public static long FactorialNum(int num) {
+        Scanner input = new Scanner(System.in);
+        if (num < 0) {
+            System.out.println("negative Number is Not allowed");
+            return 0;
+        }
+        long Mul = 1;
+        int i = 1;
+        while (i <= num) {
+            Mul *= i;
+            i++;
+        }
+        return Mul;
     }
+
 }
