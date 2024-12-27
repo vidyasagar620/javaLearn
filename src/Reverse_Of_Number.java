14258128a461ca087e73fec2a45b183b8b424b20
vidyasagar620 <145008515+vidyasagar620@users.noreply.github.com>
2024-12-27 11:18:31 +0530

    We are Learn How to  Perform Reverse Of Number  In java

---
+++ src/Reverse_Of_Number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Reverse_Of_Number {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Reverse ");
+        System.out.print("Enter Your Number: ");
+        int num = input.nextInt();
+        int sum = Reverse(num);
+        System.out.println("Your Reverse NUmber Is: " + sum);
+    }
+
+    public static int Reverse(int num ){
+        int sum = 0;
+        int i = num;
+        while (num != 0){
+            int reminder = num % 10;
+            sum = sum * 10 + reminder;
+            num = num / 10;
+
+        }
+
+        return sum;
+    }
+}
