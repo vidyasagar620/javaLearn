10035f2dca9c4852dd4ebc54168760b942664bda
vidyasagar620 <145008515+vidyasagar620@users.noreply.github.com>
2025-01-05 23:08:52 +0530

    We are Learn How to  Perform Fibonacci_Series  In java

--- src/Fibonacci_Series.java
+++ src/Fibonacci_Series.java
@@ -6,8 +6,8 @@ public class Fibonacci_Series {
         System.out.println("Welcome to Fibonacci Series\n________________________");
         System.out.print("Enter Your Number: ");
         int num = input.nextInt();
-        long fib = Fib_Num(num);
         System.out.println("Fibonacci Series up to " + num + " terms:");
+        long fib = Fib_Num(num);
 
     }
     public static int Fib_Num (int num){
